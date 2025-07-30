383013e73d707c6031e26dcc4ea10ae312d0faae
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-07-30 22:57:59 +0200

    feat: implement generate_sequence

--- show_sequence/grid_container.gd
+++ show_sequence/grid_container.gd
@@ -4,8 +4,15 @@ func flash_sequence(sequence: Array[int]):
 	for selected in sequence:
 		var selected_button: Button = get_node("Button{0}".format([selected]))
 		selected_button.text = "O"
-		await get_tree().create_timer(1).timeout
+		await get_tree().create_timer(.8).timeout
 		selected_button.text = "X"
+		
+func generate_sequence(length: int):
+	var sequence: Array[int]
+	for i in range(length):
+		sequence.append(randi_range(1, 9))
+	return sequence
 
 func _ready() -> void:
-	flash_sequence([1, 9, 4])
+	var sequence = generate_sequence(10)
+	flash_sequence(sequence)
