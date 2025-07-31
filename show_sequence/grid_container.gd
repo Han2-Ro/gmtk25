838b289dbb481b85f8d55e8aade4ae3dabc6143d
Jonas Bulik <jonas@bulik.dev>
2025-07-31 10:05:48 +0200

    refactor: separate button into scene

--- show_sequence/grid_container.gd
+++ show_sequence/grid_container.gd
@@ -2,7 +2,7 @@ extends GridContainer
 
 func flash_sequence(sequence: Array[int]):
 	for selected in sequence:
-		var selected_button: Button = get_node("Button{0}".format([selected]))
+		var selected_button: BaseButton = get_node("Button{0}".format([selected]))
 		selected_button.text = "O"
 		await get_tree().create_timer(.8).timeout
 		selected_button.text = "X"
