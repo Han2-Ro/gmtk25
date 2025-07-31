dbdce26f6f620f26d36c8a27eef72e2e7b1479b6
Jonas Bulik <jonas@bulik.dev>
2025-07-31 11:33:19 +0200

    refactor: create sequene button abstraction

--- show_sequence/grid_container.gd
+++ show_sequence/grid_container.gd
@@ -1,30 +1,29 @@
 extends GridContainer
+class_name SequenceController
 
-func flash_sequence(sequence: Array[int]):
-	for button in buttons:
-		button.disabled = true
-	for selected in sequence:
-		var selected_button: TextureButton = buttons[selected]
-		var original_modulate = selected_button.modulate
-		selected_button.modulate = Color.GRAY
-		await get_tree().create_timer(.8).timeout
-		selected_button.release_focus()
-		selected_button.modulate = original_modulate
-		await get_tree().create_timer(.2).timeout
-	for button in buttons:
-		button.disabled = false
+signal sequence_flash_start
+signal flash_button(SequenceButton)
+signal sequence_flash_end
+signal pressed_correct(SequenceButton)
+signal pressed_wrong(SequenceButton)
+signal step_completed
 
-var buttons: Array[TextureButton]
+func flash_sequence(sequence: Array[SequenceButton]):
+	sequence_flash_start.emit()
+	for step in sequence:
+		flash_button.emit(step)
+		await step.flash()
+	sequence_flash_end.emit()
 
-var level_sequence: Array[int]
-var user_presses = []
+var buttons: Array[SequenceButton]
+
+var level_sequence: Array[SequenceButton]
+var user_presses: Array[SequenceButton] = []
 
 func generate_sequence(length: int):
-	var max_index = len(buttons) - 1
-	var sequence: Array[int] = []
+	var sequence: Array[SequenceButton] = []
 	for i in range(length):
-		var next = randi_range(0, max_index)
-		sequence.append(next)
+		sequence.append(buttons.pick_random())
 	return sequence
 
 func _ready() -> void:
@@ -33,29 +32,25 @@ func _ready() -> void:
 	# this way we get almost compile time type checking
 	for button in get_children():
 		buttons.append(button)
-	
-	for button_index in range(len(buttons)):
-		var button: BaseButton = buttons[button_index]
-		button.connect("pressed", _on_button_pressed.bind(button_index))
+		button._controller_ready(self)
 		
-	level_sequence = generate_sequence(4)
+		# TODO: do not use index but actual tile
+		button.pressed.connect(_on_button_pressed.bind(button))
+		
+	level_sequence = generate_sequence(9)
 	flash_sequence(level_sequence)
 
 
-func _on_button_pressed(index: int) -> void:
+func _on_button_pressed(pressed_button: SequenceButton) -> void:
 	var current_step = len(user_presses)
-	var expected_index = level_sequence[current_step]
-	var pressed_button = buttons[index]
-	if (expected_index == index):
-		for button in buttons:
-			button.disabled = false
-			button.modulate = Color.WHITE
-		user_presses.append(index)
-		pressed_button.modulate = Color.GREEN_YELLOW
-		await get_tree().create_timer(.8).timeout
-		pressed_button.modulate = Color.WHITE
+	var expected_button = level_sequence[current_step]
+	var is_correct = expected_button == pressed_button
+	if is_correct:
+		user_presses.append(pressed_button)
+		pressed_correct.emit(pressed_button)
+		await pressed_button.this_pressed_correct()
+		step_completed.emit()
 	else:
-		# TODO: disable wrong button until next step
-		pressed_button.modulate = Color.INDIAN_RED
-		pressed_button.disabled = true
-		print("WRONG! Should have been button", expected_index)
+		print("WRONG! Should have been button", expected_button)
+		pressed_wrong.emit(pressed_button)
+		await pressed_button.this_pressed_wrong()
---
+++ show_sequence/sequence_button.gd
@@ -0,0 +1,37 @@
+extends TextureButton
+class_name SequenceButton
+
+@onready var initial_modulate = self.modulate
+
+func _controller_ready(controller: SequenceController):
+	controller.sequence_flash_start.connect(on_sequence_flash_start)
+	controller.sequence_flash_end.connect(_reset)
+	controller.step_completed.connect(on_step_completed)
+	controller.pressed_correct.connect(_on_pressed_correct_button)
+
+func on_sequence_flash_start():
+	self.disabled = true
+	
+func _on_pressed_correct_button(_btn):
+	self._reset()
+
+func flash():
+	self.modulate = Color.GRAY
+	await get_tree().create_timer(.8).timeout
+	self.modulate = initial_modulate
+	await get_tree().create_timer(.2).timeout
+
+func _reset():
+	self.disabled = false
+	self.modulate = initial_modulate
+
+func this_pressed_correct():
+	self.modulate = Color.GREEN_YELLOW
+	await get_tree().create_timer(.8).timeout
+
+func this_pressed_wrong():
+	self.modulate = Color.INDIAN_RED
+	self.disabled = true
+	
+func on_step_completed():
+	self._reset()
