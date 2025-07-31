1f660c3d01e3c8cfe53045a9145bb8913a31207a
Jonas Bulik <jonas@bulik.dev>
2025-07-31 09:31:50 +0200

    feat: check that user pressed correct button sequence

--- show_sequence/grid_container.gd
+++ show_sequence/grid_container.gd
@@ -6,7 +6,10 @@ func flash_sequence(sequence: Array[int]):
 		selected_button.text = "O"
 		await get_tree().create_timer(.8).timeout
 		selected_button.text = "X"
-		
+
+var level_sequence = generate_sequence(3)
+var user_presses = []
+
 func generate_sequence(length: int):
 	var sequence: Array[int]
 	for i in range(length):
@@ -14,9 +17,15 @@ func generate_sequence(length: int):
 	return sequence
 
 func _ready() -> void:
-	var sequence = generate_sequence(10)
-	flash_sequence(sequence)
+	flash_sequence(level_sequence)
 
 
 func _on_button_pressed(id: int) -> void:
-	print(id)
+	var current_step = len(user_presses)
+	var expected_button = level_sequence[current_step]
+	if (expected_button == id):
+		print("Correct")
+		user_presses.append(id)
+	else:
+		# TODO: disable wrong button until next step
+		print("WRONG! Should have been button", expected_button)
