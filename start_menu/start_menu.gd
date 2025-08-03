e4190e9879044bd7faaee89dff640cf66973bb22
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2025-08-03 15:01:16 +0200

    fix: camera timing

--- start_menu/start_menu.gd
+++ start_menu/start_menu.gd
@@ -5,15 +5,13 @@ signal start_game
 
 @export var start_button: Button3D
 
-
 func _ready() -> void:
 	start_button.pressed.connect(_on_start_pressed)
-
 
 func _on_start_pressed():
 	var camera: GameCamera = $"../Camera3D"
 	if camera:
-		await camera.transition_to_map()
+		camera.transition_to_map()
 	else:
 		printerr("Camera not found")
 	print("Emitting start_game")
