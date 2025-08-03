9c0dafe55d1ecfd242a1d54fb3b0e724ceb20a06
Jonas Bulik <jonas@bulik.dev>
2025-08-03 15:45:39 +0200

    feat(ui): pretty cash display

--- level_ui/cash_counter.gd
+++ level_ui/cash_counter.gd
@@ -1,5 +1,17 @@
-extends Label
+extends Control
+
+var tween: Tween
+@onready var label: Label = $MarginContainer/HBoxContainer/CashLabel
 
 
 func update_cash(amount: int) -> void:
-	self.text = "Coins: %d" % amount
+	if tween:
+		tween.kill()
+	tween = create_tween()
+	var current_value = int(label.text)
+	var duration = clamp(sqrt(abs(amount - current_value) / 10), 0.1, 0.7)
+	tween.tween_method(set_label_text, current_value, amount, duration)
+
+
+func set_label_text(value: int):
+	label.text = str(value)
